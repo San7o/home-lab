e5081edd17870c0f4b0924b489fd1ae8ce85cf7f
Giovanni Santini <santigio2003@gmail.com>
2026-05-18 23:03:43 +0200

    scripts: lei: subscribing to u-boot mailing list

--- scripts/lei-update.sh
+++ scripts/lei-update.sh
@@ -8,12 +8,19 @@ set -e
 
 STAGING_MAILBOX=~/Mail/drivers-staging
 STAGING_INBOX=https://lore.kernel.org/linux-staging/
-STAGING_QUERY='dfn:drivers/staging/ AND rt:30.days.ago..'
+#STAGING_QUERY='dfn:drivers/staging/ AND rt:30.days.ago..'
+STAGING_QUERY='(l:linux-staging.vger.kernel.org OR l:linux-staging@lists.linux.dev OR dfn:drivers/staging/) AND rt:30.days.ago..'
 
 EBPF_MAILBOX=~/Mail/bpf
 EBPF_INBOX=https://lore.kernel.org/bpf/
-EBPF_QUERY='(dfn:kernel/bpf/ OR dfn:tools/lib/bpf/ OR dfn:tools/testing/selftests/bpf/) AND rt:30.days.ago..'
+#EBPF_QUERY='(dfn:kernel/bpf/ OR dfn:tools/lib/bpf/ OR dfn:tools/testing/selftests/bpf/) AND rt:30.days.ago..'
+EBPF_QUERY='(l:bpf@vger.kernel.org OR dfn:kernel/bpf/ OR dfn:tools/lib/bpf/ OR dfn:tools/testing/selftests/bpf/) AND rt:30.days.ago..'
 
+UBOOT_MAILBOX=~/Mail/u-boot
+UBOOT_INBOX=https://lore.kernel.org/u-boot/
+UBOOT_QUERY='l:u-boot.lists.denx.de: AND rt:30.days.ago..'
+
+# Staging
 if [ ! -d $STAGING_MAILBOX ]; then
     echo "Setting up staging"
     lei q -o $STAGING_MAILBOX \
@@ -24,6 +31,7 @@ else
     lei up $STAGING_MAILBOX
 fi
 
+# eBPF
 if [ ! -d $EBPF_MAILBOX ]; then
     echo "Setting up ebpf"
     lei q -o $EBPF_MAILBOX \
@@ -32,6 +40,17 @@ if [ ! -d $EBPF_MAILBOX ]; then
 else
     echo "Fetching ebpf"
     lei up $EBPF_MAILBOX
+fi
+
+# U-boot
+if [ ! -d $UBOOT_MAILBOX ]; then
+    echo "Setting up uboot"
+    lei q -o $UBOOT_MAILBOX \
+        -I $UBOOT_INBOX \
+        --threads $UBOOT_QUERY
+else
+    echo "Fetching uboot"
+    lei up $UBOOT_MAILBOX
 fi
 
 echo "Done"
