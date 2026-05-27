1afd62a9070bd638291e35f7fd1bfe1d7e56ab42
Giovanni Santini <santigio2003@gmail.com>
2026-05-27 22:08:02 +0200

    website: renamed webpage
    Signed-off-by: Giovanni Santini <santigio2003@gmail.com>

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'home-lab-notes'
+project = 'Notes'
 author = 'Giovanni Santini'
 
 # -- General configuration ---------------------------------------------------
